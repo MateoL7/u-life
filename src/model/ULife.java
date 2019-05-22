efb0c323d95567c85a3746f23659050c298dd8c7
Juan Pablo <juanpablovelasco2@gmail.com>
2019-05-22 12:23:42 -0500

    se agrego los metodos para serealiar al igual que metodos para crear cuentas

--- src/model/ULife.java
+++ src/model/ULife.java
@@ -1,5 +1,13 @@
 package model;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class ULife {
 
 	private Account firstUser;
@@ -37,4 +45,62 @@ public class ULife {
 		}
 		return found;
 	}
+	/**
+	 * 
+	 * @param username name of the user	
+	 * @param password password of the user
+	 * @param age age of the user
+	 * @param weight weight of the user
+	 * @param height height of the user
+	 * @param gender gender of the user
+	 * @param nickName nickname of the user
+	 * @return a message indicating wether or not the user could be added
+	 */
+	public String CreateNewUser(String username, String password, int age, double weight, double height, String gender, String nickName) {
+		String message;
+		if(username == null || password == null || weight == 0 || height == 0 || gender == null || nickName == null || age == 0) {
+			message = "No se pude crear la cuenta debido a que uno o mas campos requeridos estan vacios";
+		}else {
+		Account a = new Account(username,password,age,weight,height,gender,nickName);
+		if(firstUser != null) {
+			Account temp = firstUser.getPrev();
+			temp.setNext(a);
+			firstUser.setPrev(a);
+			a.setNext(firstUser);
+			a.setPrev(temp);
+			message = "Se creo exitosamente su cuenta : " + "\n" + "Usuario : " + username + "\n" + "Contraseña : " +password;
+		}
+		else {
+			firstUser = a;
+			firstUser.setNext(a);
+			firstUser.setPrev(a);
+			message = "Se creo exitosamente su cuenta : " + "\n" + "Usuario : " + username + "\n" + "Contraseña : " +password;
+		}
+		
+		}
+		return message;
+	}
+	
+	/**
+	 * El metodo carga los objetos de account y los guarda en un archivo .mateo
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	public void SaveData() throws FileNotFoundException, IOException {
+		File f = new File("data/SavedAccounts.mateo");
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f));
+		oos.writeObject(firstUser);
+		oos.close();
+		
+	}
+	
+	/*El metodo carga los datos desde un archivo .mateo
+	 * 
+	 */
+	public void LoadData() throws FileNotFoundException, IOException, ClassNotFoundException {
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("data/SavedAccounts.mateo"));
+		firstUser = (Account)ois.readObject();
+		ois.close();
+	}
+	
 }
--- src/userInterface/RegisterController.java
+++ src/userInterface/RegisterController.java
@@ -1,9 +1,15 @@
 package userInterface;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import model.ULife;
 
 public class RegisterController {
 
@@ -33,9 +39,30 @@ public class RegisterController {
 
     @FXML
     private Button registration;
+    
+    private ULife ulife;
 
+    public void initialize() {
+    	ulife = new ULife();
+    }
     
     public void doneRegistration(ActionEvent e) {
-    	System.out.println("Works");
+    	String username = nameT.getText();
+    	String password = passwordT.getText();
+    	int age = Integer.parseInt(ageT.getText());
+    	double weight = Double.parseDouble(weightT.getText());
+    	double height =  Double.parseDouble(heightT.getText());
+    	String gender = genderT.getText();
+    	String nickName = nicknameT.getText();
+    	JOptionPane.showMessageDialog(null,ulife.CreateNewUser(username, password, age, weight, height, gender, nickName));
+    	try {
+			ulife.SaveData();
+		} catch (FileNotFoundException e1) {
+		    JOptionPane.showMessageDialog(null, "no se pudo encontar el archivo");
+		
+		} catch (IOException e1) {
+		JOptionPane.showMessageDialog(null, " hubo un error");
+			
+		}
     }
 }
